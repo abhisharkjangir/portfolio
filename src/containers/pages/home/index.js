62e7b3afec7416fbfed20b2e599f62cd3bd2ee78
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2020-04-10 16:56:48 +0530

    Remove unnecessary files

--- src/containers/pages/home/index.js
+++ src/containers/pages/home/index.js
@@ -1,5 +1,1 @@
 export { default } from './home';
-export * from './actions';
-export { default as sagas } from './saga';
-export { default as reducer } from './reducers';
-export { HOME_STATE_KEY as homeStateKey } from './constants';
--- src/containers/pages/home/selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-import { HOME_STATE_KEY } from './constants';
-
-export const homeState = state => state.get(HOME_STATE_KEY);
-
-export const isUsersFetching = () =>
-  createSelector(homeState, state => state.get('isFetching'));
-export const getUsersData = () =>
-  createSelector(homeState, state => state.get('data').toJS());
