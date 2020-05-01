a2a29315ba948b4aa236429e41386d26d230d7cf
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2020-05-01 22:13:41 +0530

    Moving scroll logic to home page from app component

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './home.scss';
 import Hero from '../../hero/hero';
 import About from '../../about/about';
@@ -8,19 +8,31 @@ import Work from '../../work/work';
 import Projects from '../../projects/projects';
 import Contact from '../../contact/contact';
 
-class Home extends React.PureComponent {
-  render() {
-    return (
-      <div id="home" className={styles.home}>
-        <Hero />
-        <About />
-        <Jobs />
-        <Work />
-        <Projects />
-        <Contact />
-      </div>
-    );
-  }
-}
+const Home = () => {
+  // Scroll into view for hash location
+  const location = useLocation();
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.substring(1); // location.hash without the '#'
+      setTimeout(() => {
+        const element = document.getElementById(id);
+        if (element) {
+          element.scrollIntoView();
+        }
+      }, 100);
+    }
+  });
+
+  return (
+    <div id="home" className={styles.home}>
+      <Hero />
+      <About />
+      <Jobs />
+      <Work />
+      <Projects />
+      <Contact />
+    </div>
+  );
+};
 
 export default Home;
--- src/containers/app/app.jsx
+++ src/containers/app/app.jsx
@@ -1,22 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { useLocation } from 'react-router-dom';
 import AppComponent from '../../components/app';
 
 const App = props => {
-  // Scroll into view for hash location
-  const location = useLocation();
-  useEffect(() => {
-    if (location.hash) {
-      const id = location.hash.substring(1); // location.hash without the '#'
-      setTimeout(() => {
-        const element = document.getElementById(id);
-        if (element) {
-          element.scrollIntoView();
-        }
-      }, 100);
-    }
-  });
   return <AppComponent {...props} />;
 };
 
