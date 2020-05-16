70d1f4e5f94a5cf3eb95335c6cc657c4667366d5
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2020-05-16 19:11:01 +0530

    SimpleIcon and Heading Component and timeSince util

---
+++ src/components/common/heading/heading.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Heading = ({ level, className, children }) => {
+  const Tag = `h${level}`;
+  return <Tag className={className}>{children}</Tag>;
+};
+
+Heading.propTypes = {
+  children: PropTypes.object.isRequired,
+  level: PropTypes.any,
+  className: PropTypes.string,
+};
+
+Heading.defaultProps = {
+  level: 3,
+  className: '',
+};
+
+export default Heading;
