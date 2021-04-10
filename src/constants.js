573eeade5d9192f94a77b7feae921042853a52a1
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2021-04-10 14:11:09 +0530

    correcting URLs

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,19 @@
 export const socialList = [
   {
     name: 'github',
-    url: 'https:abhisharjangir.com/apis/redirect/github',
+    url: 'https://abhisharjangir.com/apis/redirect/github',
   },
   {
     name: 'linkedin',
-    url: 'https:abhisharjangir.com/apis/redirect/linkedin',
+    url: 'https://abhisharjangir.com/apis/redirect/linkedin',
   },
   {
     name: 'instagram',
-    url: 'https:abhisharjangir.com/apis/redirect/instagram',
+    url: 'https://abhisharjangir.com/apis/redirect/instagram',
   },
   {
     name: 'twitter',
-    url: 'https:abhisharjangir.com/apis/redirect/twitter',
+    url: 'https://abhisharjangir.com/apis/redirect/twitter',
   },
 ];
 
