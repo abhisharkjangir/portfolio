f5be8c219aa3c777d63cdfdb7376957a8c428b4d
Abhishar Jangir <abhishar.jangir@idc.bedbath.com>
2021-06-05 11:10:58 +0530

    New color pallete and css variables cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      primary: '#fece2f',
+      primaryLight: '#fece2f33',
+      secondary: '#000000',
+      secondaryLight: '#0a0a0a',
+      secondaryLighter: '#151515',
+      secondaryLightest: '#92929233',
+      tertiary: '#929292',
+      tertiaryLight: '#d4d4d4',
+      tertiaryLigher: '#e6f1ff',
+    },
+    blend: 'screen',
   },
   variants: {
     extend: {},
