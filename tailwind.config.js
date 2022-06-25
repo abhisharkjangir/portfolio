d18a6002362a4f69acb1fca8d0cb71ed1598d1cf
Abhishar Jangir <abhisharjangir@ymail.com>
2022-06-25 13:25:10 +0530

    tailwing upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,5 @@
 module.exports = {
-  purge: {
-    preserveHtmlElements: false,
-    content: ['./src/**/*.html', './src/**/*.js', './src/**/*.jsx'],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.jsx'],
   theme: {
     extend: {},
     colors: {
