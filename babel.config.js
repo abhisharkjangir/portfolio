7e11795760f6024abb2dbc1be8bf0a97d3ade3cf
Abhishar Jangir <abhisharjangir@ymail.com>
2022-06-26 16:18:49 +0530

    updating and removing un neccessory babel plugins

--- babel.config.js
+++ babel.config.js
@@ -5,9 +5,6 @@ module.exports = api => {
     plugins: [
       '@babel/plugin-syntax-dynamic-import',
       '@loadable/babel-plugin',
-      '@babel/plugin-syntax-import-meta',
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-json-strings',
       [
         '@babel/plugin-proposal-decorators',
         {
@@ -15,11 +12,9 @@ module.exports = api => {
         }
       ],
       '@babel/plugin-proposal-function-sent',
-      '@babel/plugin-proposal-export-namespace-from',
       '@babel/plugin-proposal-numeric-separator',
       '@babel/plugin-proposal-throw-expressions',
       '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-logical-assignment-operators',
       '@babel/plugin-proposal-optional-chaining',
       [
         '@babel/plugin-proposal-pipeline-operator',
@@ -27,7 +22,6 @@ module.exports = api => {
           proposal: 'minimal'
         }
       ],
-      '@babel/plugin-proposal-nullish-coalescing-operator',
       '@babel/plugin-proposal-do-expressions',
       '@babel/plugin-proposal-function-bind'
     ],
