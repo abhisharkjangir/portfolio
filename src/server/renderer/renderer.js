c04dc60ea8bb1574f55a4d23765da934f8fec351
Abhishar Jangir <abhisharjangir@ymail.com>
2022-07-30 22:06:55 +0530

    Removing IIFE Code

--- src/server/renderer/renderer.js
+++ src/server/renderer/renderer.js
@@ -20,84 +20,82 @@ import {
 } from './renderHelper';
 
 let htmlFileData;
-// LOADER
-export default ({ clientStats }) =>
-  (req, res) => {
-    (async () => {
-      const history = createMemoryHistory({
-        initialEntries: [`${req.path}?ssr=true`],
-      });
-      // Create a store (with a memory history) from our current url
-      const { store } = createStore(history);
-      const context = {};
-      const extractor = new ChunkExtractor({ stats: clientStats });
 
-      const actions = RouteList.reduce((routeActions, route) => {
-        getRouteActions(route, req, routeActions);
-        return routeActions;
-      }, []);
+const renderer = async (req, res, clientStats) => {
+  res.setHeader('Cache-Control', 'max-age=86400');
+  const history = createMemoryHistory({
+    initialEntries: [`${req.path}?ssr=true`],
+  });
+  // Create a store (with a memory history) from our current url
+  const { store } = createStore(history);
+  const context = {};
+  const extractor = new ChunkExtractor({ stats: clientStats });
 
-      const promises = actions.map((action) => store.dispatch(action()));
+  const actions = RouteList.reduce((routeActions, route) => {
+    getRouteActions(route, req, routeActions);
+    return routeActions;
+  }, []);
 
-      // Helmet Action to set meta info based on the URL
-      if (Meta[req.url]) {
-        promises.push(store.dispatch(setHelmetInfo(Meta[req.url])));
+  const promises = actions.map((action) => store.dispatch(action()));
+
+  // Helmet Action to set meta info based on the URL
+  if (Meta[req.url]) {
+    promises.push(store.dispatch(setHelmetInfo(Meta[req.url])));
+  }
+
+  return Promise.allSettled(promises)
+    .then(async () => {
+      const markup = renderToString(
+        <ChunkExtractorManager extractor={extractor}>
+          <StrictMode>
+            <Provider store={store}>
+              <StaticRouter location={req.url} context={context}>
+                <App />
+              </StaticRouter>
+            </Provider>
+          </StrictMode>
+        </ChunkExtractorManager>
+      );
+
+      if (context.url) {
+        // If context has a url property, then we need to handle a redirection in Redux Router
+        res.writeHead(302, {
+          Location: context.url,
+        });
+        res.end();
+      } else {
+        // Otherwise, we carry on...
+        // We need to tell Helmet to compute the right meta tags, title, and such
+        const helmet = Helmet.renderStatic();
+        const state = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
+        const preloadScripts = extractor
+          .getLinkTags()
+          ?.split('\n')
+          ?.filter((link) => link?.includes('as="script"'))
+          ?.join('');
+        // Pass all this nonsense into our HTML formatting function above
+
+        if (!htmlFileData) {
+          htmlFileData = await readHtmlFileData(res);
+        }
+        const styleElements = await extractor.getStyleElements();
+        const html = injectHTML(htmlFileData, {
+          html: helmet.htmlAttributes.toString(),
+          title: helmet.title.toString(),
+          meta: helmet.meta.toString(),
+          body: markup,
+          scripts: extractor.getScriptTags(),
+          style: `<style>${getInlineCss(styleElements)}</style>`,
+          state,
+          preloadScripts,
+          theme: req.path === '/test/theme/light' ? 'light' : 'dark',
+        });
+        res.send(html);
       }
+    })
+    .catch((error) => console.error('::::: Error :::::', error));
+};
 
-      return Promise.allSettled(promises)
-        .then(async () => {
-          const markup = renderToString(
-            <ChunkExtractorManager extractor={extractor}>
-              <StrictMode>
-                <Provider store={store}>
-                  <StaticRouter location={req.url} context={context}>
-                    <App />
-                  </StaticRouter>
-                </Provider>
-              </StrictMode>
-            </ChunkExtractorManager>
-          );
-
-          if (context.url) {
-            // If context has a url property, then we need to handle a redirection in Redux Router
-            res.writeHead(302, {
-              Location: context.url,
-            });
-            res.end();
-          } else {
-            // Otherwise, we carry on...
-            // We need to tell Helmet to compute the right meta tags, title, and such
-            const helmet = Helmet.renderStatic();
-            const state = JSON.stringify(store.getState()).replace(
-              /</g,
-              '\\u003c'
-            );
-            const preloadScripts = extractor
-              .getLinkTags()
-              ?.split('\n')
-              ?.filter((link) => link?.includes('as="script"'))
-              ?.join('');
-            // Pass all this nonsense into our HTML formatting function above
-
-            if (!htmlFileData) {
-              htmlFileData = await readHtmlFileData(res);
-            }
-            const styleElements = await extractor.getStyleElements();
-            const html = injectHTML(htmlFileData, {
-              html: helmet.htmlAttributes.toString(),
-              title: helmet.title.toString(),
-              meta: helmet.meta.toString(),
-              body: markup,
-              scripts: extractor.getScriptTags(),
-              style: `<style>${getInlineCss(styleElements)}</style>`,
-              state,
-              preloadScripts,
-              theme: req.path === '/test/theme/light' ? 'light' : 'dark',
-            });
-            res.setHeader('Cache-Control', 'max-age=86400');
-            res.send(html);
-          }
-        })
-        .catch((error) => console.error('::::: Error :::::', error));
-    })();
-  };
+export default ({ clientStats }) =>
+  (req, res) =>
+    renderer(req, res, clientStats);
