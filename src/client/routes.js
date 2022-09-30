b2f331e4532fdbfb3cde330a15a402b51250f4b4
Abhishar Jangir <abhisharjangir@ymail.com>
2022-09-30 21:10:17 +0530

    removing extra standalone and test apps

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,7 +1,5 @@
 import portfolioRoutes from '@modules/portfolio/portfolioRoutes';
-import testRoutes from '@modules/portfolio/testRoutes';
-import standAloneAppRoutes from '@modules/standAloneApp/standAloneAppRoutes';
 
-const RouteList = [portfolioRoutes, testRoutes, standAloneAppRoutes];
+const RouteList = [portfolioRoutes];
 
 export default RouteList;
